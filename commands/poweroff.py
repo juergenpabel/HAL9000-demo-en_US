d6933f8a0f1b9bd7deded5db8c0c18a2c2421c4e
Juergen Pabel <juergen@pabel.net>
2024-05-08 11:50:39 +0200

    brain: switched to python scripts using dbus-fast for poweroff/reboot via dbus

---
+++ commands/poweroff.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from dbus_fast.aio import MessageBus
+from dbus_fast.auth import AuthExternal, UID_NOT_SPECIFIED
+from dbus_fast.constants import BusType
+import asyncio
+
+async def main():
+	bus = await MessageBus(None, BusType.SYSTEM, AuthExternal(UID_NOT_SPECIFIED)).connect()
+	introspection = await bus.introspect('org.freedesktop.login1', '/org/freedesktop/login1')
+	obj = bus.get_proxy_object('org.freedesktop.login1', '/org/freedesktop/login1', introspection)
+	manager = obj.get_interface('org.freedesktop.login1.Manager')
+	await manager.call_power_off(True)
+
+asyncio.run(main())
+
